23f24b0158389b990c1f0a03e8ab696b57010468
Elykopter <knovikov2003@inbox.ru>
2022-11-25 12:54:25 +0300

    feat(main): add files lab-5

---
+++ lab05/lab5.asm
@@ -0,0 +1,19 @@
+; lab05.asm
+SECTION .data
+	hello:	DB 'Новиков Константин',10
+	
+	helloLen: EQU $-hello
+	
+SECTION .text
+	GLOBAL _start
+	
+_start:
+	mov eax,4
+	mov ebx,1
+	mov ecx,hello
+	mov edx,helloLen
+	int 80h
+	
+	mov eax,1
+	mov ebx,0
+	int 80h
